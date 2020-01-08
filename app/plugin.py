a7b7b279455759de10511d931763ab186e3e0abb
dnanto <dnegron2@gmu.edu>
2020-01-08 10:04:27 -0500

    removing this...

--- app/plugin.py
+++ app/plugin.py
@@ -5,24 +5,6 @@ import signal
 import sys
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 
-# def countabilityProof():
-#     # generates portion of result from Cantor's diagonalization argument
-#     floatList = []
-#     result = ''
-#     for i in range(50):
-#         # excludes 0, to comply with proof requirement
-#         num = 0
-#         while float(num) == 0:
-#             num = random.random()
-#         floatList.append('{0:.50f}'.format(num))
-# 
-#     for i in range(2, 50):
-#         if int(floatList[i][i]) == 0:
-#             result += str(1)
-#         else:
-#             result += str(0)
-# 
-#     return result
 
 def bernoulli_process(p="0.5"):
     p = float(p)
@@ -34,8 +16,7 @@ def bernoulli_process(p="0.5"):
 # functions only accept keyword arguments (kwargs)
 # functions are responsible for parsing kwarg types from string
 choices = {
-    "bernoulli_process": bernoulli_process,
-    # "countability_proof": countability_proof
+    "bernoulli_process": bernoulli_process
 }
 
 def generate_sequence(func, n, kwargs):
